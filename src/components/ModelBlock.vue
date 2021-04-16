b95524e1b5ebb5ee25a4e47bccf68626c235f399
natcha <natchaphannoi@gmail.com>
2021-04-17 00:04:04 +0700

    62130500124 [Add] views

---
+++ src/components/ModelBlock.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container box-content h-50 w-auto overflow-hidden border bg-green-light bg-opacity-95 rounded-lg shadow p-4 m-5">
+    <slot></slot>
+    </div>
+</template>
---
+++ src/views/Contact.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>The event4</h1>
+    <div class="border rounded-lg bg-white mx-20 py-8">
+      <h1 class="text-green-darkgreen text-3xl font-bold">Contact Us</h1>
+      <div class="text-green-darkgreen font-bold">
+        Contact to send information for public relations
+      </div>
+      <h1 class="text-green-dark text-3xl font-bold mt-5">Address</h1>
+      <p>51/96 myhome shiracha chonburi 20111</p>
+      <h1 class="text-green-dark text-3xl font-bold mt-5">
+        Phone number and Email
+      </h1>
+      <p>Tel : 099-7899964</p>
+      <p>Email : natchaphannoi@gmail.com</p>
+    </div>
+
+</template>
+<script>
+export default {
+  name: "Contact",
+  components: {},
+};
+</script>
+
+
