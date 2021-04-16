88c2920e82d82d5386c712f93b6c4980e2af3042
natcha <natchaphannoi@gmail.com>
2021-04-17 00:00:30 +0700

    62130500124  [Add] news

---
+++ src/components/News.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <h3 class="text-brown-dark text-3xl font-black">| News </h3>
+    <div class="  bg-cream-light border-solid border-opacity-75 rounded-lg mx-20 mt-4">
+      <div class="flex flex-initial grid grid-cols-4  ">
+     <box>
+        <div class="font-bold text-left text-sm" >Jonah Marais & Daniel Seavey Speak On The Future Of The Band, New Music</div>
+        <div class="text-left ">SO I know we don’t really post any candid stuff, but since Daniel and Jonah decided to talk to the paparazzi and since we haven’t seen them in what feels like forever (haha) I decided to post it. Enjoy! </div>
+      </box>
+           <box>
+        <h1>Why don't we</h1>
+        <div>ปล่อยเพลงใหม่</div>
+      </box>
+         <box>
+        <h1>Why don't we</h1>
+        <div>ปล่อยเพลงใหม่</div>
+      </box>
+         <box>
+        <h1>Why don't we</h1>
+        <div>ปล่อยเพลงใหม่</div>
+      </box>
+      
+      </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'News'
+}
+</script>
