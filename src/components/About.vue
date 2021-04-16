b6215a7abf2931f47b0333b15d7f5e0960e6bb68
natcha <natchaphannoi@gmail.com>
2021-04-17 00:01:13 +0700

    62130500124 [Add] About

---
+++ src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="grid grid-cols-2  bg-brown-light mt-28">
+    <div class="border bg-grayblue mx-5 my-6 p-10 ">
+    <h2 class="font-bold text-base">About activities information</h2>
+     <p class="text-sm text-left">The Event4 is a website that disseminates news and activities related to singers and artists in Thailand. Without being able to organize the camp by himself If you have any questions about the activity, please inquire directly with the camp organizer.</p>
+</div>
+<div class="border bg-grayblue mx-5 my-6 p-10">
+    <h2 class="font-bold text-base">About news on the Event4</h2>
+     <p class="text-sm text-left">The information on this website is the copyright of its author, it has been compiled from reputable sources. No part or all of the site may be copied without prior written permission.</p>
+
+</div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'About'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="pt-10">EVENT4</h1>
+    <!-- แทรกรูป -->
   </div>
+  <div class="mt-10 ">
+   <news></news>
+  <div class="mt-5">
+    <h3 class="text-brown-dark text-3xl font-black">| Activities</h3>
+    <div class=" bg-cream-light border-solid border-opacity-75 rounded-lg mx-20 mt-4">
+      <div class="flex flex-initial grid grid-cols-3  ">
+      </div>
+    </div>
+    </div>
+</div>
+  <About > </About>
+
+<footer>
+    <span>Created By <a href="#">criss</a> | <span class="fa fa-copyright">
+    </span> 2020 All rights reserved.</span>
+</footer>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import About from "@/components/About.vue";
+import News from '@/components/News.vue';
+
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    About, News
+  },
+};
 </script>
