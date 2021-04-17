c6d877d97d10fa18cc25fecbdf17285e0afeef5f
natcha <natchaphannoi@gmail.com>
2021-04-17 20:38:11 +0700

    62130500124 [Update] Add backend script for run serve

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Event from '../views/Event.vue'
 import Contact from '../views/Contact.vue'
+import Event from '../views/Event'
 
 const routes = [
   {
@@ -13,8 +13,7 @@ const routes = [
     path: '/event',
     name: 'Event',
     component: Event
-  }
-  ,
+  },
   {
     path: '/contact',
     name: 'Contact',
