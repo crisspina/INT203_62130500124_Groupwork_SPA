ee10c1d6f891313436dde4c9a9ae239a822373d2
natcha <natchaphannoi@gmail.com>
2021-04-17 20:39:16 +0700

    62130500124 [Update] edit tailwind css

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,32 +1,49 @@
 <template>
-
-    <h3 class="text-brown-dark text-3xl font-black">| News </h3>
-    <div class="  bg-cream-light border-solid border-opacity-75 rounded-lg mx-20 mt-4">
-      <div class="flex flex-initial grid grid-cols-4  ">
-     <box>
-        <div class="font-bold text-left text-sm" >Jonah Marais & Daniel Seavey Speak On The Future Of The Band, New Music</div>
-        <div class="text-left ">SO I know we don’t really post any candid stuff, but since Daniel and Jonah decided to talk to the paparazzi and since we haven’t seen them in what feels like forever (haha) I decided to post it. Enjoy! </div>
-      </box>
-           <box>
-        <h1>Why don't we</h1>
-        <div>ปล่อยเพลงใหม่</div>
-      </box>
-         <box>
-        <h1>Why don't we</h1>
-        <div>ปล่อยเพลงใหม่</div>
-      </box>
-         <box>
-        <h1>Why don't we</h1>
-        <div>ปล่อยเพลงใหม่</div>
-      </box>
-      
+  <h3 class="text-brown-dark text-3xl font-black">| News</h3>
+  <div
+    class="bg-cream-light border-solid border-opacity-75 rounded-lg mx-20 mt-4">
+    <div class="flex flex-initial grid grid-cols-4">
+      <div class="container box-content h-50 w-auto overflow-hidden border bg-green-light bg-opacity-95 rounded-lg shadow p-4 m-5">
+        <div class="font-bold text-left text-sm">
+          Jonah Marais & Daniel Seavey Speak On The Future Of The Band, New
+          Music
+        </div>
+        <div class="text-left">
+          SO I know we don’t really post any candid stuff, but since Daniel and
+          Jonah decided to talk to the paparazzi and since we haven’t seen them
+          in what feels like forever (haha) I decided to post it. Enjoy!
+        </div>
+      </div>
+      <div class="container box-content h-50 w-auto overflow-hidden border bg-green-light bg-opacity-95 rounded-lg shadow p-4 m-5">
+        <div class="font-bold text-left text-sm">
+          New Music of King.
+        </div>
+        <div class="text-left">
+          Suppose end get boy warrant general natural. Delightful met sufficient projection ask. Decisively everything principles if preference do impression of. Preserved oh so difficult repulsive on in household. In what do miss time be. 
+        </div>
+      </div>
+      <div class="container box-content h-50 w-auto overflow-hidden border bg-green-light bg-opacity-95 rounded-lg shadow p-4 m-5">
+        <div class="font-bold text-left text-sm">
+          New Composes.
+        </div>
+        <div class="text-left">
+          Manor we shall merit by chief wound no or would. Oh towards between subject passage sending mention or it.
+        </div>
+      </div>
+      <div class="container box-content h-50 w-auto overflow-hidden border bg-green-light bg-opacity-95 rounded-lg shadow p-4 m-5">
+        <div class="font-bold text-left text-sm">
+          Wishing -babe
+        </div>
+        <div class="text-left">
+          Wise busy past both park when an ye no. Nay likely her length sooner thrown sex lively income.
+        </div>
       </div>
     </div>
-
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'News'
-}
+  name: "News",
+};
 </script>
