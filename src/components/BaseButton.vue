c118ae267ca23917942b21b9c0745e485835ea23
natcha <natchaphannoi@gmail.com>
2021-04-18 14:07:04 +0700

    62130500124 [Add] add components-basebutton(props)

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="py-2 px-2 font-bold rounded-md border hover:bg-brown-light hover:text-brown-dark " 
+    :class="[buttonColor, textColor, borderColor]" 
+    :type="buttonType"> {{ buttonLabel }}
+    </button>
+</template>
+<script>
+export default {
+      name: 'BaseButton',
+    props: ['buttonLabel','buttonColor','textColor','borderColor','buttonType'],
+}
+</script>
+
