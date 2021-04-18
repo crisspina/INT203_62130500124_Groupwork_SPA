712f13818fdb20409b91d3ac499d74f0ccfb6d4c
natcha <natchaphannoi@gmail.com>
2021-04-18 11:04:17 +0700

    ุ62130500124 [update] edit event.vue css

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,23 +1,31 @@
 <template>
+<div class="logo mt-10">The event4</div> 
+<div class="border bg-white opacity-70 rounded-lg p-5 mt-2 lg:mx-60 sm:mx-5 ">
+        <p class="text-xl text-black font-extrabold pb-3 mt-5">Agreement for the transmission of the activity information</p>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+    </div>
+<div class="text-2xl text-green-darkgreen font-extrabold mt-5 ">___________________________________________________________________</div> 
+<h2 class="text-2xl text-green-darkgreen font-extrabold pb-3 mt-7">Add Event</h2>
   <div class="">
-    <h1 class="text-green-darkgreen text-3xl font-bold">Event</h1>
     <form @submit.prevent="addEvent">
-      <h2 class="text-center text-2xl font-extrabold pb-5">Add The Event</h2>
-      <div class="pb-2">
+      <div class="mt-5">
         <label class="" for="title"> Title:</label>
         <input
-          class="text-black rounded-lg text-sm w-auto focus:outline-none py-0.5 pl-3"
+          class="text-black rounded-lg text-sm focus:outline-none py-0.5 w-80 pl-3 "
+          cols=""
           id="title"
           type="text"
           v-model="enteredTitle"
-          placeholder="Enter your Event Tiltle"
+          placeholder="Enter your Event Name"
           required
         />
       </div>
       <div class="pb-2">
-        <h2 class="pb-1" for="description">Details about the activity.</h2>
+        <h2 class="pb-1 pt-4 text-bold" for="description">Details about the activity:</h2>
         <textarea
-          class="focus:outline-none text-black text-sm pl-3 pt-1 rounded-lg w-full"
+          class="focus:outline-none text-black text-sm pl-3 pt-1 rounded-lg"
+          cols="100"
+          rows="9"
           id="description"
           type="text"
           v-model="enteredDescription"
@@ -27,9 +35,7 @@
       </div>
       <!-- submit but -->
       <div class="pt-2 pl-5 mx-48">
-        <button
-          class="focus:outline-none btn text-sm bg-green-darkgreen text-white rounded-2xl shadow px-3 py-2 my-1"
-        >
+        <button class="focus:outline-none btn text-sm bg-green-darkgreen text-white rounded-2xl shadow px-3 py-2 my-1" >
           submit
         </button>
       </div>
@@ -113,7 +119,9 @@
 <script>
 export default {
   name: "Event",
-  components: {},
+  components: {
+    
+  },
 
   emits: ["btn-click"],
 
@@ -129,26 +137,6 @@ export default {
     };
   },
   methods: {
-    // async addEvent() {
-    //   const newEvent = {
-    //     title: this.enteredTitle,
-    //     description: this.enteredDescription,
-    //   };
-    //   const res = await fetch(this.url, {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-type": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       title: newEvent.title,
-    //       description: newEvent.description,
-    //     }),
-    //   });
-    //   const data = await res.json();
-    //   this.AddEventShow = [...this.AddEventShow, data];
-    //   this.enteredTitle = "";
-    //   this.enteredDescription = "";
-    // },
     async addEvent() {
       const newEvent = {
         title: this.enteredTitle,
@@ -221,4 +209,15 @@ export default {
   },
 };
 </script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap');
+.logo {
+  font-family: 'Dancing Script' ;
+  font-size: 70px;
+  color: #6b705c;
+  font-weight: 600 ;
+}
+</style>
+
+
 
